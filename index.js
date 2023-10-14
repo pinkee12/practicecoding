57cbcf3ea72c6eacf23ee57a1dd8fbe0a99a86a5
Pinkee Maurya <akpipinkymaurya@gmail.com>
2023-10-14 14:49:48 +0530

    file is added

---
+++ index.js
@@ -0,0 +1,17 @@
+function checkEvenOdd(N){
+    if(N%2==0){
+        console.log("Even")
+    }
+    else{
+        console.log("Odd")
+    }
+    if(N%2==1){
+        console.log("Odd")
+    }
+    else{
+        console.log("Even")
+    }
+
+}
+
+
